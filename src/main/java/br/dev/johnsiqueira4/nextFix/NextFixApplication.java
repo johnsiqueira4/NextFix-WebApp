0fbe2d56ffe3f04a58e17d911f5abc2af963fa86
johnsiqueira4 <brenosiqueira.4ndroid@gmail.com>
2025-10-19 01:04:17 -0300

    first commit

---
+++ src/main/java/br/dev/johnsiqueira4/nextFix/NextFixApplication.java
@@ -0,0 +1,13 @@
+package br.dev.johnsiqueira4.nextFix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextFixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/johnsiqueira4/nextFix/NextFixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.johnsiqueira4.nextFix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
