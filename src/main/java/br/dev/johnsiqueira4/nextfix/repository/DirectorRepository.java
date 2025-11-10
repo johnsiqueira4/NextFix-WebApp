cbc127ff6a1ea37f6e874f64cf42b3c72e4ec199
johnsiqueira4 <johnsiqueira4@users.noreply.github.com>
2025-11-10 21:39:46 +0000

    deploy: 742ee42a3d185d282198fcc4a4f6e1d9e4589307

--- src/main/java/br/dev/johnsiqueira4/nextfix/repository/DirectorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.dev.johnsiqueira4.nextfix.repository;
-
-import br.dev.johnsiqueira4.nextfix.models.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DirectorRepository extends JpaRepository<Director, Long> {
-}
